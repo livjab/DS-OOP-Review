2e2e8310a464094aff8e22f0ade958ded975b437
Liv Johnson <livjab@gmail.com>
2019-05-29 17:41:45 -0500

    added more teams, locations, and weeks

--- football/possible_values.py
+++ football/possible_values.py
@@ -1,4 +1,7 @@
 team_names = ['LA Chargers', 'LA Rams', 'NE Patriots', 'NY Giants',
-              'Chicago Bears']
+              'Chicago Bears', 'MN Vikings', 'Green Bay Packers',
+              'Dallas Cowboys', 'Seattle Seahawks']
 # TODO
 # Add possible locations and weeks in the season
+locations = ['Los Angeles', 'Chicago', 'Minneapolis', 'Seattle', 'Dallas']
+weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
