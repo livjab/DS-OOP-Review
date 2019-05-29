76592cc7c3268045d4e08ce4489a2c5c760b2870
Liv Johnson <livjab@gmail.com>
2019-05-29 16:53:50 -0500

    forgot : when creating class

--- football/teams.py
+++ football/teams.py
@@ -1,4 +1,4 @@
 class team_names:
-    def __init__(self)
+    def __init__(self):
         team_names = ['LA Chargers', 'LA Rams', 'NE Patriots', 'NY Giants',
                       'Chicago Bears']
